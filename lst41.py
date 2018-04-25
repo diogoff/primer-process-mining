4b09d4f896b6779b2b8988603d14bb5e31d8ca8f
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2018-04-25 20:28:59 +0100

    Add files via upload

---
+++ lst41.py
@@ -0,0 +1,31 @@
+import datetime
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('financial_log.xes')
+root = tree.getroot()
+
+ns = {'xes': 'http://www.xes-standard.org/'}
+
+for trace in root.findall('xes:trace', ns):
+    caseid = ''
+    for string in trace.findall('xes:string', ns):
+        if string.attrib['key'] == 'concept:name':
+            caseid = string.attrib['value']
+    for event in trace.findall('xes:event', ns):
+        task = ''
+        user = ''
+        event_type = ''
+        for string in event.findall('xes:string', ns):
+            if string.attrib['key'] == 'concept:name':
+                task = string.attrib['value']
+            if string.attrib['key'] == 'org:resource':
+                user = string.attrib['value']
+            if string.attrib['key'] == 'lifecycle:transition':
+                event_type = string.attrib['value']
+        timestamp = ''
+        for date in event.findall('xes:date', ns):
+            if date.attrib['key'] == 'time:timestamp':
+                timestamp = date.attrib['value']
+                timestamp = datetime.datetime.strptime(timestamp[:-10],
+                                                                    '%Y-%m-%dT%H:%M:%S')
+        print ';'.join([caseid, task, event_type, user, str(timestamp)])
