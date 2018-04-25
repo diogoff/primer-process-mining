2b9345b42d03b88e63ca49edc48e24d1ed283460
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2018-04-25 19:57:18 +0100

    Add files via upload

---
+++ lst11.py
@@ -0,0 +1,16 @@
+from lst08 import *
+
+F = dict()
+for caseid in log:
+    for i in range(0, len(log[caseid])-1):
+        ai = log[caseid][i][0]
+        aj = log[caseid][i+1][0]
+        if ai not in F:
+            F[ai] = dict()
+        if aj not in F[ai]:
+            F[ai][aj] = 0
+        F[ai][aj] += 1
+
+for ai in sorted(F.keys()):
+    for aj in sorted(F[ai].keys()):
+        print ai, '->', aj, ':', F[ai][aj]
